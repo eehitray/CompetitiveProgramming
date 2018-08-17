e606c77f0b950a8fcefc4046206f9b6093ee6f42
FenrisWolf123 <bhargav.kulkarni1@gmail.com>
2018-08-17 19:29:32 +0530

    keywords added

---
+++ Syntax Stuff/keywords.py
@@ -0,0 +1,38 @@
+# stuff we use a lot
+and
+in
+else
+from
+if
+continue
+not
+pass
+while
+is
+break
+return
+elif
+def
+import
+for
+or
+lambda
+class
+True
+False
+None
+
+# stuff we use not that much
+try
+global
+with
+as
+yield
+except
+raise
+del
+finally
+
+# stuff we dont use (as of now that is)
+nonlocal
+assert
